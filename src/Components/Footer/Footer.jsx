376dfd18048b4a42739488ebe29d20f57e1e1ac6
Pratyush Sahu <68014145+PratyushSahu27@users.noreply.github.com>
2024-07-08 05:02:08 +0530

    shoormall logo update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 
-import footer_logo from "../Assets/logo_big.png";
+import footer_logo from "../Assets/shoormall-logo-final-transparent.png";
 import instagram_icon from "../Assets/instagram_icon.png";
 import whatsapp_icon from "../Assets/whatsapp_icon.png";
 import { useNavigate } from "react-router-dom";
@@ -13,7 +13,6 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-logo">
         <img src={footer_logo} alt="" />
-        <p>Shoora Mall</p>
       </div>
       <ul className="footer-links">
         {/* <li>Products</li> */}
@@ -46,9 +45,6 @@ const Footer = () => {
         <div className="footer-icons-container">
           <img src={instagram_icon} alt="" />
         </div>
-        {/* <div className="footer-icons-container">
-          <img src={pintrest_icon} alt="" />
-        </div> */}
         <div className="footer-icons-container">
           <img src={whatsapp_icon} alt="" />
         </div>
