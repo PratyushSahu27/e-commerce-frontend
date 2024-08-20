e03c65fe9e6052947586ca4c33d2e9a3035e4d2c
Pratyush Sahu <68014145+PratyushSahu27@users.noreply.github.com>
2024-08-20 23:53:26 +0530

    fix import

--- src/Components/AddToCartButton/AddToCartButton.jsx
+++ src/Components/AddToCartButton/AddToCartButton.jsx
@@ -1,4 +1,3 @@
-import "./AddToCartButton.scss";
 import React, { useContext, useState } from "react";
 import { Button, IconButton, Box } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
